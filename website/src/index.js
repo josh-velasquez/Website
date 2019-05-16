647e3df7f98e95267988e78273a7ec6ca3d9824e
Josh Velasquez <jvelasquez@zephyrsleep.com>
2019-05-16 14:25:58 -0600

    Started on react website

---
+++ website/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePageLayout from "./components/HomePageLayout";
+
+ReactDOM.render(<HomePageLayout />, document.querySelector("#root"));

06604a757fd32c4a6a984bb8d1e22a8df64245c4
Josh <joshvelasqez6@gmail.com>
2020-11-03 00:18:45 -0700

    Finished animations for fading in. Working on the project cards

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,6 @@
 $(window).on("load", function() {
     $(window).scroll(function() {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight() - 200;
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight() - 150;
         $(".fade").each(function() {
             var objectBottom = $(this).offset().top + $(this).outerHeight();
             if (objectBottom < windowBottom) {
@@ -74,3 +74,26 @@ window.onload = function() {
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
 };
+
+
+function showImages(el) {
+    var windowHeight = jQuery(window).height();
+    $(el).each(function() {
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos) {
+            $(this).addClass("fadeIn");
+        }
+    });
+}
+
+// if the image in the window of browser when the page is loaded, show that image
+$(document).ready(function() {
+    showImages('.fade');
+});
+
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function() {
+    showImages('.fade');
+});
